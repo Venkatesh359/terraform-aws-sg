033c6b5a39a086fbd8278b1ec7fff30fa76c1c78
Venkatesh <tvenky359@gmail.com>
2025-10-30 20:15:27 +0530

    egress

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "aws_security_group" "main" {
   name        = var.sg_name
   description = var.sg_description
   vpc_id      = var.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = merge(        # Tags are merged from multiple sources: user-defined tags, local tags, and a dynamic Name tag.
     var.sg_tags,       # Custom VPC tags passed via variables
@@ -11,3 +17,5 @@ resource "aws_security_group" "main" {
     }
   )
 }
+
+
